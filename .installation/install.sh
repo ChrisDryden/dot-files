352bfbeec5cd17e61297d23cae50d3c65b9da591
Chris Dryden <christopher.paul.dryden@gmail.com>
2019-08-20 16:14:17 -0400

    Updating install.sh

--- .installation/install.sh
+++ .installation/install.sh
@@ -5,6 +5,11 @@ brew cask install iterm2
 brew install vim
 brew upgrade vim
 
+brew install cmake
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+
 if ! [ -n "$ZSH_VERSION" ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 	git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
